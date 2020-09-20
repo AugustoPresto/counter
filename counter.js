42c95dab081a0b8863a1df72b8b9debb6d215bfe
Augusto Souza <augustosouza2222@gmail.com>
2020-09-20 18:20:11 -0300

    declared function reset

---
+++ counter.js
@@ -0,0 +1,12 @@
+let count = document.querySelector('h2')
+
+function reset() {
+	if (count.innerHTML != 0) {
+		count.innerHTML = 0
+		count.style.color = 'black'
+	}
+}
+
+function decrease() {
+
+}
